4ed68eafe15d0297ada6253ff88ad5186b8f4acb
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-09-02 08:43:36 +1000

    feat(Api): Add Database connection to API

--- backend/src/api/Api.ts
+++ backend/src/api/Api.ts
@@ -1,5 +1,6 @@
 import express, { Application } from "express";
 import { Server } from "http";
+import { Database } from "../db/Database";
 import { Router } from "../routes/Router";
 
 export class Api {
@@ -8,9 +9,15 @@ export class Api {
 
   constructor(private port: number) {
     this.app = express();
+    this.connectDatabase();
     this.setHeaders();
     this.connectMiddleware();
     this.connectRoutes();
+  }
+
+  private connectDatabase(): void {
+    const db = new Database();
+    db.connect();
   }
 
   private setHeaders(): void {
