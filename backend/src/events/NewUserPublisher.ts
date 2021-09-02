5a096081ebad9bff5e09678860daf87a3ad01ebc
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-09-03 08:28:18 +1000

    feat(backend): Add publishNewUser method
    - Add method for publishing a new user on NewUserPublisher

--- backend/src/events/NewUserPublisher.ts
+++ backend/src/events/NewUserPublisher.ts
@@ -1,3 +1,4 @@
+import { UserDocument } from "../api/users/users.model";
 import { EventPublisher } from "./EventPublisher";
 
 const NEW_USER_TOPIC = "NEW_USER";
@@ -5,5 +6,11 @@ const NEW_USER_TOPIC = "NEW_USER";
 export class NewUserPublisher extends EventPublisher {
   constructor() {
     super(NEW_USER_TOPIC);
+  }
+
+  public async publishNewUser(user: UserDocument): Promise<void> {
+    const jsonString = JSON.stringify(user);
+    const buffer = Buffer.from(jsonString);
+    return await this.publish(buffer);
   }
 }
