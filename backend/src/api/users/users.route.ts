110d4ce6a7709e43b3fbb2e66e4a84d2930f9762
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-09-04 18:24:24 +1000

    fix(backend): Fix user route controller usage

--- backend/src/api/users/users.route.ts
+++ backend/src/api/users/users.route.ts
@@ -4,10 +4,13 @@ import { UserController } from "./users.controller";
 const users = Router();
 const controller = new UserController();
 
-users.get("/", controller.getUsers);
-users.get("/:id", controller.getUserById);
-users.post("/", controller.createUser);
-users.put("/:id", controller.updateUserById);
-users.delete("/:id", controller.deleteUser);
+users.get("/", async (req, res) => await controller.getUsers(req, res));
+users.get("/:id", async (req, res) => await controller.getUserById(req, res));
+users.post("/", async (req, res) => await controller.createUser(req, res));
+users.put(
+  "/:id",
+  async (req, res) => await controller.updateUserById(req, res)
+);
+users.delete("/:id", async (req, res) => await controller.deleteUser(req, res));
 
 export { users };
