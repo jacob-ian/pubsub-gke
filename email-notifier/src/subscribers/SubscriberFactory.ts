221f9d1a7dac8efb18d9ee2d83c035427b243543
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-09-10 09:20:53 +1000

    refactor(email-notifier): Use maps in factory
    - Use a subscriberMap to create the right subscriber in the factory

--- email-notifier/src/subscribers/SubscriberFactory.ts
+++ email-notifier/src/subscribers/SubscriberFactory.ts
@@ -1,22 +1,25 @@
 import { AbstractSubscriber } from "./AbstractSubscriber";
 import { NewUserSubscriber } from "./new-user/NewUserSubscriber";
-import { NullSubscriber } from "./NullSubscriber";
 
-enum Subscribers {
-  NEW_USER = "new_user",
-}
+const subscriberMap = {
+  newUser: NewUserSubscriber,
+};
 
+type Subscriber = keyof typeof subscriberMap;
 export class SubscriberFactory {
   constructor() {}
 
-  public async getSubscriber(type: Subscribers): Promise<AbstractSubscriber> {
-    let subscriber: AbstractSubscriber;
-    if (type === Subscribers.NEW_USER) {
-      subscriber = new NewUserSubscriber();
-    } else {
-      subscriber = new NullSubscriber();
-    }
+  public async getSubscriber(type: Subscriber): Promise<AbstractSubscriber> {
+    const subscriber = new subscriberMap[type]();
     await subscriber.init();
     return subscriber;
+  }
+
+  public async getSubscribers(): Promise<AbstractSubscriber[]> {
+    return Promise.all(
+      Object.keys(subscriberMap).map((subscriber) =>
+        this.getSubscriber(subscriber as Subscriber)
+      )
+    );
   }
 }
