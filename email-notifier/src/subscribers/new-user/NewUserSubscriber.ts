b2655dc80b86bf6651e3fa05b299df6dd15dc2f4
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-09-10 17:39:27 +1000

    feat(email-notifier): It's alive!
    - Send "processing" status on message handle to pubsub
    - Verify message is valid, or send 400
    - Fetch the user data from the message
    - Fix #1

--- email-notifier/src/subscribers/new-user/NewUserSubscriber.ts
+++ email-notifier/src/subscribers/new-user/NewUserSubscriber.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { AbstractSubscriber } from "../AbstractSubscriber";
+import { Message } from "@google-cloud/pubsub";
 
 export class NewUserSubscriber extends AbstractSubscriber {
   constructor() {
@@ -10,5 +11,20 @@ export class NewUserSubscriber extends AbstractSubscriber {
     });
   }
 
-  public async handleRequest(req: Request, res: Response): Promise<void> {}
+  public async handleRequest(req: Request, res: Response): Promise<void> {
+    const { message } = req.body;
+    if (!this.verifyMessage(message)) {
+      res
+        .send(400)
+        .json({ error: "bad_request", message: "A bad request was made." });
+      return;
+    }
+    res.sendStatus(102);
+    const user = message.data.toString();
+    console.log(`New User: ${user}`);
+  }
+
+  private verifyMessage(message: any): message is Message {
+    return !!message.data && !!message.messageId;
+  }
 }
