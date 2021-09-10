ddbd13ae6b0e4f389726947151d559b1daf85d45
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-09-11 09:58:39 +1000

    refactor(email-notifier): Remove old files

--- email-notifier/src/newUser/new-user.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-import { Message } from "@google-cloud/pubsub";
-
-export async function emailNewUser(req: Request, res: Response): Promise<void> {
-  res.sendStatus(102);
-  const { data } = req.body as Message;
-  const user = JSON.parse(data.toString());
-  console.log(user);
-}
