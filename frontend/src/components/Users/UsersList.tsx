6d187e059996a58db7cd254e5c57d69593f429b0
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-09-12 20:32:27 +1000

    feat(frontend): Implement users service in usersList

--- frontend/src/components/Users/UsersList.tsx
+++ frontend/src/components/Users/UsersList.tsx
@@ -1,46 +1,51 @@
+import { ReactNode, useEffect, useState } from "react";
 import styles from "./UsersList.module.sass";
-import { HTTPMethods, useApi } from "../../hooks/useApi";
+import { UserDocument, UsersService } from "../../services/Users.service";
 import User from "./User";
 
-export interface UserDocument {
-  _id: string;
-  name: {
-    first: string;
-    last: string;
-  };
-  email: string;
-}
-
 export default function UsersList() {
-  const { data, error, loading } = useApi({
-    method: HTTPMethods.GET,
-    url: "/users",
-  });
+  const [users, setUsers] = useState<UserDocument[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<any>();
 
-  function handleDelete(id: string) {
-    console.log(`delete ${id}`);
+  useEffect(() => {
+    setLoading(true);
+    UsersService.getAll()
+      .then(({ data }) => {
+        setUsers(data.data.users);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false);
+      });
+  }, []);
+
+  async function handleDelete(id: string) {
+    try {
+      await UsersService.delete(id);
+      setUsers((state) => state.filter((user) => user._id !== id));
+    } catch (err) {
+      setError(err);
+    }
   }
 
-  let content = <div>Something went wrong!</div>;
+  let userEls = users.map((user) => (
+    <User
+      key={user._id}
+      firstName={user.name.first}
+      lastName={user.name.last}
+      email={user.email}
+      handleDelete={() => handleDelete(user._id)}
+    />
+  ));
 
+  let content: ReactNode;
   if (!loading) {
     if (error) {
       content = <div>{JSON.stringify(error, null, 2)}</div>;
-    } else if (data) {
-      content =
-        data.users?.length > 0 ? (
-          data.users.map((user: any) => (
-            <User
-              key={user._id}
-              firstName={user.name.first}
-              lastName={user.name.last}
-              email={user.email}
-              handleDelete={() => handleDelete(user._id)}
-            />
-          ))
-        ) : (
-          <div>There are no users!</div>
-        );
+    } else if (users) {
+      content = users.length > 0 ? userEls : <div>There are no users!</div>;
     }
   }
 
