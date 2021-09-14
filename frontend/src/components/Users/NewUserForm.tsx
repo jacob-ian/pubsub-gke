24e75c9c8e2d5b964b96011700b0e5e12e2e147e
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-09-14 20:28:38 +1000

    style(frontend): Create grid-able form
    - Create a form-field class

--- frontend/src/components/Users/NewUserForm.tsx
+++ frontend/src/components/Users/NewUserForm.tsx
@@ -9,27 +9,35 @@ interface NewUserFormProps {
 export default function NewUserForm(props: NewUserFormProps) {
   return (
     <form className={styles.form}>
-      <label htmlFor="fname">First Name:</label>
-      <input
-        id="fname"
-        value={props.value.fname}
-        name="fname"
-        onChange={props.onChange}
-      />
-      <label htmlFor="lname">Last Name:</label>
-      <input
-        id="lname"
-        value={props.value.lname}
-        name="lname"
-        onChange={props.onChange}
-      />
-      <label htmlFor="email">Email Address:</label>
-      <input
-        id="email"
-        name="email"
-        value={props.value.email}
-        onChange={props.onChange}
-      />
+      <div className={styles.group}>
+        <div className={styles["form-field"]}>
+          <label htmlFor="fname">First Name:</label>
+          <input
+            id="fname"
+            value={props.value.fname}
+            name="fname"
+            onChange={props.onChange}
+          />
+        </div>
+        <div className={styles["form-field"]}>
+          <label htmlFor="lname">Last Name:</label>
+          <input
+            id="lname"
+            value={props.value.lname}
+            name="lname"
+            onChange={props.onChange}
+          />
+        </div>
+      </div>
+      <div className={styles["form-field"]}>
+        <label htmlFor="email">Email Address:</label>
+        <input
+          id="email"
+          name="email"
+          value={props.value.email}
+          onChange={props.onChange}
+        />
+      </div>
     </form>
   );
 }
